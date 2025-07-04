669c3ea2c76e58f46778305ad1f9df0c633c0bed
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-07-03 23:09:32 -0300

    Inserção na função do arquivo userValidations.js

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -1,7 +1,32 @@
 const {body} = require("express-validator");
 
 const userCreateValidation = () => {
-    return [body("name").isString().withMessage("O nome é obrigatório. ")]
+    return [
+     body("name")
+        .isString()
+        .withMessage("O nome é obrigatório.")
+        .isLength({min:3})
+        .withMessage("O nome precisa ter no mínimo três caracteres."),
+      body("email")
+        .isString()
+        .withMessage("O e-mail é obrigatório.")
+        .isEmail()
+        .withMessage("Insira um e-mail válido."),
+      body("password")
+      .isString()
+      .withMessage("A senha é obrigatória")
+      .isLength({min:5})
+      .withMessage("Asenha precisa ter no mínimo 5 caracteres."),
+      body("confirmpassword")
+      .isString()
+      .withMessage("A confirmação de senha é obrigatória.")
+      .custom((value,{req}) => {
+        if (value != req.body.password) {
+            throw new Error("As senhas não são iguais.")
+        }
+        return true;
+      })
+    ];
 };
 
 module.exports = {

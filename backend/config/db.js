925fe53899c9e92a85041f7641f395d2cddf3893
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-07-07 15:28:46 -0300

    Inserções no arquivo db.json

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,23 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.zzyh0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        );
+
+        console.log("Conectou ao banco de dados!");
+
+        return dbConn
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+conn();
+
+module.exports = conn;
 
 //connection

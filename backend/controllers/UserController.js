e6ed71ccb3796730f49dd7b1c042d14da42b878a
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-09-16 17:00:20 -0300

    Alteração e criação de função no arquivo UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -76,7 +77,45 @@ const getCurrentUser = async(req,res) =>{
 
 //Update an user
 const update = async(req,res) =>{
-  res.send("Update");
+  //res.send("Update");
+
+  const {name,password,bio} = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename
+  }
+
+  const reqUser = req.user
+  
+  const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select("-password")
+
+  // const user = await User.findById(id).select('-password');
+
+  if (name) {
+    user.name = name
+  }
+
+  if (password) {
+    //Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password,salt);
+
+    user.password = passwordHash
+  }
+
+  if(profileImage) {
+    user.profileImage = profileImage
+  }
+
+  if(bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
+
+  res.status(200).json(user);
 };
 
 module.exports = {

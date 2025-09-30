7455df3f72f0119f0c9a002f7457041edf125900
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-09-29 22:41:38 -0300

    Inserção no arquivo Router.js

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express();
 
-router.use("/api/users",require("./UserRouter"))
+router.use("/api/users",require("./UserRouter"));
+router.use("/api/photos",require("./PhotoRouter"));
 
 //teste route
 router.get("/",(req,res) =>{

3a4e72a587b77488d0de2d0318adff4303b3d010
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-11-05 22:22:28 -0300

    Inserção no arquivo PhotoController.js

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -88,7 +88,17 @@ const deletePhoto = async(req,res) =>{
     res.status(200).json({id:photo._id,message:"Foto excluida com sucesso."});
 }; 
 
+//Get all photos
+const getAllPhotos = async(req,res) =>{
+ const photos = await Photo.find({})
+ .sort([["createdAt",-1]])
+ .exec()
+
+ return res.status(200).json(photos);
+ 
+}
 module.exports = {
     insertPhoto,
     deletePhoto,
+    getAllPhotos,
 };

de47cf0d33a0d356b92b6f491e39aa7355aa85d4
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-11-12 22:27:59 -0300

    Alterações na função do arquivo UserRouter.js

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,3 +1,40 @@
+// const express = require("express");
+// const router = express.Router();
+
+// //Controller
+// const {register,
+//     getCurrentUser,
+//     login,
+//     update,
+//     getUserById,
+// } = require("../controllers/UserController");
+
+// //Middlewares
+// const validate = require("../middlewares/handleValidation");
+// const {userCreateValidation,
+//       loginValidation,
+//       userUpdateValidation,
+// } = require("../middlewares/userValidations");
+// const authGuard = require("../middlewares/authGuard");
+// const { imageUpload } = require("../middlewares/imageUpload");
+
+// //Routes
+// router.post("/register",userCreateValidation(),validate,register);
+// router.get("/profile",authGuard,getCurrentUser);
+// router.post("/login",loginValidation(),validate,login);
+// router.put(
+//     "/",
+//     authGuard,
+//     userUpdateValidation(),
+//     // validate,
+//     imageUpload.single("profileImage"),
+//     update
+// );
+
+// router.get("/:id",getUserById);
+
+// module.exports = router
+
 const express = require("express");
 const router = express.Router();
 
@@ -33,5 +70,5 @@ router.put(
 
 router.get("/:id",getUserById);
 
-module.exports = router
+module.exports = router;
 

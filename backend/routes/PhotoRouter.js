51abd106f742dc446dcac6a32c995f0a192ae8cb
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-01-29 17:14:39 -0300

    inserção no arquivo PhotoRouter.js

--- backend/routes/PhotoRouter.js
+++ backend/routes/PhotoRouter.js
@@ -6,11 +6,12 @@ const {insertPhoto,
     deletePhoto,
     getAllPhotos,
     getUserPhotos,
-    getPhotoById
+    getPhotoById,
+    updatePhoto
     } = require("../controllers/PhotoController");
 
 //Middlewares
-const {photoInsertValidation} = require("../middlewares/photoValidation");
+const {photoInsertValidation, photoUpdateValidation} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const {imageUpload} = require("../middlewares/imageUpload");
@@ -28,6 +29,7 @@ router.delete("/:id",authGuard,deletePhoto);
 router.get("/",authGuard,getAllPhotos);
 router.get("/user/:id",authGuard,getUserPhotos);
 router.get("/:id",authGuard,getPhotoById);
+router.put("/:id",authGuard,photoUpdateValidation(),validate,updatePhoto);
 
 module.exports = router;
 //res.status(404).json({id:photo._id,message:"Foto não encontrada!"}); 

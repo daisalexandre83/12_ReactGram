a992cc3e75799ea0893f9ca6253a1fa04868ccbf
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-01-30 22:31:51 -0300

    Alterações no arquivo app.js

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 
+// app.post('/tasks',(req,res) =>{
+//     const {title} = req.body;
+// })
+
 // Solve CORS
 app.use(cors({credentials:true,origin:"http://localhost:5000"}));
 

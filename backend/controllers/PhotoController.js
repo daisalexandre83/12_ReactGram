3586e8c574adcb964c80dd4ecc1fe881febf5336
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-02-24 22:27:27 -0300

    Atualização no arquivo PhotoController.js

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,4 +1,6 @@
 const Photo = require("../models/Photo");
+
+
 const User = require("../models/User");
 
 const mongoose = require("mongoose");
@@ -188,6 +190,39 @@ const likePhoto = async(req,res) =>{
     res.status(200).json({photoId:id,userId:reqUser._id,message:"A foto foi curtida"});
 };
 
+//Coment functionality
+const commentPhoto = async (req,res) => {
+    const {id} = req.params;
+    const {comment} = req.body;
+
+    const reqUser = req.user;
+
+    const photo = await Photo.findById(id)
+
+    //Check if photo exists
+    if(!photo){
+        res.status(404).json({errors:["Foto não encontrada"]})
+        return;
+    } 
+
+    //Put comment in the array of comments
+    const userComment = {
+        comment,
+        userName: user.name,
+        userImage:  user.profileImage,
+        userId:user._id,
+    };
+
+    photo.comments.push(userComment)
+
+    await photo.save()
+
+    res.status(200).json({
+        comment:userComment,
+        message:"O comentário foi adicionado com sucesso!"
+    })
+};
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -196,4 +231,5 @@ module.exports = {
     getPhotoById,
     updatePhoto,
     likePhoto,
+    commentPhoto
 };

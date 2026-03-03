057bc3ef8af49d4117c52ab741de6d4e0c8967da
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-03-02 22:58:43 -0300

    Inserção no arquivo PhotoController.js

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -241,6 +241,16 @@ const commentPhoto = async (req,res) => {
      }
     };
 
+    //Search photos by title
+    const searchPhotos = async(req,res) =>{
+
+       const {q} = req.query;
+
+       const photos = await Photo.find({title: new RegExp(q,"i")}).exec()
+
+       res.status(200).json(photos);
+    };
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -249,5 +259,6 @@ module.exports = {
     getPhotoById,
     updatePhoto,
     likePhoto,
-    commentPhoto
+    commentPhoto,
+    searchPhotos,
 };

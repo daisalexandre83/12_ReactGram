e654265e98de18acafc5a93ee3549741567fc06c
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-03-26 21:39:51 -0300

    Inserção no arquivo App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
 
 //Router
-import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
+
+
 
 //Pages
 import Home from "./pages/Home/Home";
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+//Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+      <Navbar/>
        <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
-        <Route path="register" element={<Register/>}></Route>
+        <Route path='/register' element={<Register/>}/>
        </Routes>
+      <Footer/>  
      </BrowserRouter>
     </div>
   );

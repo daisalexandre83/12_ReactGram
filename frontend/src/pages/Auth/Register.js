7b55c96564dd2f2a0829d3335d7caa23248dd41e
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-04-01 18:47:09 -0300

    Inserção de função no arquivo Register.js da pasta frontend

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,7 +1,31 @@
 import "./Auth.css";
 
+//Components
+import {Link, link} from 'react-router-dom';
+
+//Hooks
+import { useState,useEffect } from "react";
+
 const Register = () =>{
-    return <div>Register</div>;
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+
+    return <div>
+      <h2>Register</h2> 
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p> 
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome"/>
+        <input type="email" placeholder="E-mail"/>
+        <input type="password" placeholder="Senha"/>
+        <input type="password" placeholder="Confirme a senha"/>
+        <input type="submit" value="Cadastrar"/>
+      </form>
+      <p>
+        Já tem conta?<Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>;
 };
 
 export default Register;

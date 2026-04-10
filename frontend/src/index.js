139c61a8fb7d543770f9e81b8fa10212b5d7b6fd
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-04-09 23:31:51 -0300

    Inserção no arquivo index.js da pasta frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Redux
+import {Provider} from "react-redux"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

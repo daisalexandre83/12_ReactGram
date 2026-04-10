b45ad31a3d584858d8fdfea44a0b89703873533a
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-04-10 19:06:25 -0300

    Criação do arquivo config.js da pasta frontend

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,8 @@
+export const api  = "http://localhost:3000/api";
+export const uploads = "http://localhost:3000/uploads";
+
+export const requestConfig = (method,data,token = null,image = null)=>{
+
+   let config
+
+}

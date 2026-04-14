b3a742c06211eef60601afe40ad6c8af0a16cbfd
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-04-13 21:37:10 -0300

    inserção no arquivo config.js da pasta frontend

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -5,4 +5,30 @@ export const requestConfig = (method,data,token = null,image = null)=>{
 
    let config
 
+   if (image) {
+      config = {
+         method,
+         body:data,
+         headers:{}
+      }
+   } else if(method === "DELETE" || data === null){
+      config = {
+         method,
+         headers:{},
+      };
+   }else{
+      config = {
+         method,
+         body:JSON.stringify(data),
+         headers:{
+            "Content-Type":"application/json"
+         }
+      }
+   }
+
+   if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+   }
+
+  return config;
 }

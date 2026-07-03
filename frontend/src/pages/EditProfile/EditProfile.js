86ef23bb0f44323da4403178d21a823b620e46c3
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-07-02 21:56:20 -0300

    Inserção e criação função no arquivo EditProfile.js

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -17,9 +17,13 @@ const EditProfile = () => {
 
     const {user,message,error,loading} = useSelector((state) => state.user)
     
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
+    const [name,setName] = useState("")
+    const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
+    const [profileImage,setImageProfile] = useState("")
+    const [bio,setBio] = useState("")
+    const [previewImage,setPreviewImage] = useState("")
+
     // states
 
     // Load user data
@@ -27,29 +31,46 @@ const EditProfile = () => {
         dispatch(profile());
     },[dispatch]);
 
-    console.log(user);
+    // Fill form with user data
+    useEffect(() =>{
+        if (user) {
+          setName(user.name);
+          setEmail(user.email);
+          setBio(user.bio);  
+        }
+    }, [user])
 
     const handleSubmit = (e) =>{
         e.preventDefault()
+    }
+
+    const handleFile = () =>{
+        // image preview
+        const image  = e.target.files[0]
+
+        setPreviewImage(image);
+
+        // update image state
+        setImageProfile(image);
     }
   return <div id="edit-profile">
      <h2>Edite seus dados</h2>
      <p className="subtitle">Adicione uma foto de prefil e conte mais sobre você...</p>
      {/* preview da imagem*/}
      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome"/>
-        <input type="email" placeholder="E-mail" disabled/>
+        <input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ""}/>
+        <input type="email" placeholder="E-mail" disabled value={email || ""}/>
         <label>
             <span>Imagem do Perfil</span>
-            <input type="file"/>
+            <input type="file" onChange={handleFile}/>
         </label>
         <label>
             <span>Bio:</span>
-            <input type="text" placeholder="Descrição do perfil"/>
+            <input type="text" placeholder="Descrição do perfil" onChange={(e) => setBio(e.target.value)} value={bio || ""}/>
         </label>
         <label>
             <span>Quer alterar sua senha?</span>
-            <input type="password" placeholder="Digite sua nova senha"/>
+            <input type="password" placeholder="Digite sua nova senha" onChange={(e) => setPassword(e.target.value)} value={password || ""}/>
         </label>
         <input type="submit" value="Atualizar"/>
      </form>

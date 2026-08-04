a0d10d48e1a28e1587ad4fde4e93368424cd07a7
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-08-03 22:19:04 -0300

    Inserção no arquivo Profile.js

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,9 +1,22 @@
 import "./Profile.css";
 
+import { uploads } from "../../utils/config";
+
+// components
+import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+import { BsFillEyeFill, BsPencilFill, BsXlg } from "react-icons/bs";
+
+// hooks
+import { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+
+
+
 const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
-}
+    return <div>Profile</div>;
+};
 
 export default Profile

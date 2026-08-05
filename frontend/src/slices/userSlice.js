0f922af9831ca588097a1758db83030152b06a02
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-08-04 22:58:25 -0300

    Inserção no arquivo userSlice.js

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -43,7 +43,17 @@ export const updateProfile = createAsyncThunk(
 
       return data;  
     }
-)
+);
+
+// Get user details
+export const getUserDetails = createAsyncThunk(
+   "user/get",
+   async(id,thunkAPI) =>{
+     const data = await userService.getUserDetails(id);
+     
+     return data;
+   }
+);
 
 export const userSlice = createSlice({
     name:"user",
@@ -86,6 +96,16 @@ export const userSlice = createSlice({
                 state.loading = false;
                 state.error = action.payload;
                 state.user = {};
+            })
+            .addCase(getUserDetails.pending,(state)=>{
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(getUserDetails.fulfilled,(state,action) => {
+                state.loading = false;
+                state.sucess = true;
+                state.error = null;
+                state.user = action.payload;
             })
     },
 });

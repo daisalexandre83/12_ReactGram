5dd549971340a8ee9db0d0016afa83c8b27d7448
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-08-07 22:06:14 -0300

    Alterações no authGuard.js

--- backend/middlewares/authGuard.js
+++ backend/middlewares/authGuard.js
@@ -7,7 +7,10 @@ const authGuard = async(req,res,next) =>{
     const token = authHeader && authHeader.split(" ")[1];
 
     // check if header has a token
-    if(!token) return res.status(401).json({errors:["Acesso negado!"]});
+    if(!token) {
+      console.log("-> authGuard: Token não fornecido no cabeçalho Authorization");
+      return res.status(401).json({errors:["Acesso negado!"]});
+    }
 
     //check if token is valid
     try {

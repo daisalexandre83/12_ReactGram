def3727450a36f48a184910aafee71a47f27002e
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-09-01 12:16:27 -0300

    Alterações no PhotoRouter.js

--- backend/routes/PhotoRouter.js
+++ backend/routes/PhotoRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //Controller
-const {insertPhoto,
+const { insertPhoto,
     deletePhoto,
     getAllPhotos,
     getUserPhotos,
@@ -11,34 +11,34 @@ const {insertPhoto,
     likePhoto,
     commentPhoto,
     searchPhotos,
-    } = require("../controllers/PhotoController");
+} = require("../controllers/PhotoController");
 
 //Middlewares
 const {
-    photoInsertValidation, 
+    photoInsertValidation,
     photoUpdateValidation,
-     commentValidation,
-    } = require("../middlewares/photoValidation");
+    commentValidation,
+} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
-const {imageUpload} = require("../middlewares/imageUpload");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 //Routes
 router.post(
-"/",
-authGuard,
-imageUpload.single("image"),
-photoInsertValidation(),
-validate,
-insertPhoto
+    "/",
+    authGuard,
+    imageUpload.single("image"),
+    photoInsertValidation(),
+    validate,
+    insertPhoto
 );
-router.delete("/:id",authGuard,deletePhoto);
-router.get("/",authGuard,getAllPhotos);
-router.get("/user/:id",authGuard,getUserPhotos);
-router.get("/search",authGuard,searchPhotos);
-router.get("/:id",authGuard,getPhotoById);
-router.put("/:id",authGuard,photoUpdateValidation(),validate,updatePhoto);
-router.put("/like/:id",authGuard,likePhoto);
+router.delete("/:id", authGuard, deletePhoto);
+router.get("/", authGuard, getAllPhotos);
+router.get("/user/:id", getUserPhotos);
+router.get("/search", authGuard, searchPhotos);
+router.get("/:id", authGuard, getPhotoById);
+router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
+router.put("/like/:id", authGuard, likePhoto);
 router.put(
     "/comment/:id",
     authGuard,

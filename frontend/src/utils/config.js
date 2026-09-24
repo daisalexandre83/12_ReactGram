5f536f663ea665864a1ca23b0f20072cef269bb5
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-09-24 17:53:00 -0300

    alterações no config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,27 +1,27 @@
-export const api  = "http://localhost:5000/api";
+export const api = "http://localhost:5000/api";
 export const uploads = "http://localhost:5000/uploads";
 
-export const requestConfig = (method,data,token = null,image = null)=>{
+export const requestConfig = (method, data = null, token = null, image = null) => {
 
-   let config
+   let config;
 
    if (image) {
       config = {
-         method,
-         body:data,
-         headers:{}
-      }
-   } else if(method === "DELETE" || method === "GET" || data === null){
-      config = {
-         method,
-         headers:{},
+         method: method,
+         body: data,
+         headers: {},
       };
-   }else{
+   } else if (method === "DELETE" || method === "GET" || data === null) {
       config = {
          method,
-         body:JSON.stringify(data),
-         headers:{
-            "Content-Type":"application/json"
+         headers: {},
+      };
+   } else {
+      config = {
+         method,
+         body: JSON.stringify(data),
+         headers: {
+            "Content-Type": "application/json"
          }
       }
    }
@@ -30,5 +30,5 @@ export const requestConfig = (method,data,token = null,image = null)=>{
       config.headers.Authorization = `Bearer ${token}`
    }
 
-  return config;
+   return config;
 }
